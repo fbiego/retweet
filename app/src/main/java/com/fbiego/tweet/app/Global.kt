76ece899b615b40e8d357e13fab77ac3586e0aec
fbiego <fbiego.fb@gmail.com>
2021-03-20 09:00:57 +0300

    updated

---
+++ app/src/main/java/com/fbiego/tweet/app/Global.kt
@@ -0,0 +1,16 @@
+package com.fbiego.tweet.app
+
+fun lastPlayed(millis: Long): String{
+    val sec = (System.currentTimeMillis() - millis)/1000
+    return when {
+        sec >= 3600 -> {
+            "${sec/3600}h ${(sec%3600)/60}m"
+        }
+        sec >= 60 -> {
+            "${sec/60}m ${sec%60}s"
+        }
+        else -> {
+            "${sec}s"
+        }
+    }
+}
---
+++ app/src/main/java/com/fbiego/tweet/app/TweetAdapter.kt
@@ -0,0 +1,45 @@
+package com.fbiego.tweet.app
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.fbiego.tweet.R
+
+class TweetAdapter(myData: ArrayList<TweetData>): RecyclerView.Adapter<TweetAdapter.DataHolder>(){
+    private val data = mutableListOf<TweetData>()
+    init {
+        data.addAll(myData)
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DataHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.tweet_item, parent, false)
+        return DataHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: DataHolder, position: Int) {
+        holder.bind(data[position])
+    }
+
+    override fun getItemCount(): Int = data.size
+
+    fun update(dataList: ArrayList<TweetData>){
+        this.data.clear()
+        this.data.addAll(dataList)
+        this.notifyDataSetChanged()
+    }
+
+    class DataHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        private val mOrigin: TextView = itemView.findViewById(R.id.originText)
+        private val mTime: TextView = itemView.findViewById(R.id.timeText)
+        private val mRetweet: TextView = itemView.findViewById(R.id.retweetText)
+
+        fun bind(data: TweetData){
+            mOrigin.text = data.origin
+            mTime.text = lastPlayed(data.time)
+            mRetweet.text = data.retweet
+        }
+
+    }
+}
